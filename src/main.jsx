8726431fa241ccdb0ba9dd4d9acfc307d0e32a74
Nandani Thakur <nandanith7563@gmail.com>
2024-08-23 16:09:39 +0530

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 
 import ReactDOM from 'react-dom/client';
-import { configureStore } from '@reduxjs/toolkit';
+
 import { Provider } from 'react-redux';
 import store from '../src/component/store';
 import App from './App';
